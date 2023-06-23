2c58540141ff1b1dbf4efa910ae46d9ab2f6fd7f
Shamly-Shanawaz <113548974+Shamly-Shanawaz@users.noreply.github.com>
2023-06-23 15:46:11 +0530

    Update LiIcon.js

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -13,9 +13,9 @@ export default function LiIcon({reference}) {
         <svg className='-rotate-90 md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px]' width="75" height="75" viewBox='0 0 100 100'>
           <circle cx="75" cy="50" r="20" className='stroke-primary stroke-1 dark:stroke-primaryDark fill-none'/>
           <motion.circle cx="75" cy="50" r="20" className=' stroke-[5px] fill-light dark:fill-dark'
-            style={{
-              pathLength:scrollYProgress,
-            }}
+            // style={{
+            //   pathLength:scrollYProgress,
+            // }}
           />
           <circle cx="75" cy="50" r="10" className='stroke-1 fill-primary dark:fill-primaryDark'/>
         </svg>
